10da93f81186faec19c87b93c4ec1b080ec7e06d
victoria-brami <57358809+victoria-brami@users.noreply.github.com>
2023-07-11 05:03:16 +0100

    Update gen_eval.py

--- code/gen_eval.py
+++ code/gen_eval.py
@@ -136,7 +136,7 @@ if __name__ == '__main__':
 
     wandb_init(config)
     wandb.log({f'summary/samples_test': wandb.Image(grid_imgs)})
-    metric, metric_list = get_eval_metric(samples, avg=True)
+    metric, metric_list = get_eval_metric(samples, output_path=output_path, avg=True)
     metric_dict = {f'summary/pair-wise_{k}':v for k, v in zip(metric_list[:-2], metric[:-2])}
     metric_dict[f'summary/{metric_list[-2]}'] = metric[-2]
     metric_dict[f'summary/{metric_list[-1]}'] = metric[-1]
